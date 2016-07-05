f07acbaee861fcf10b8cbd0264c18e99993976ae
Lennard Spokowski <lennard.spokowski@gmail.com>
2016-07-05 14:06:56 -0400

    Initial Commit with Gitignore file

---
+++ Assets/Art/UI/ButtonJiggle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonJiggle : MonoBehaviour {
+
+    public Vector3 targetAngle = new Vector3(0f, 0f, 0f);
+
+    public Vector3 originAngle = new Vector3(0f, 0f, 0f);
+
+    private Vector3 currentAngle;
+
+    void Start()
+
+    {
+        currentAngle = transform.eulerAngles;
+
+        StartCoroutine(MoveToOrigin());
+    }
+
+    void Update()
+    {
+        if (currentAngle.x > originAngle.x)
+        StartCoroutine(MoveToOrigin());
+        Debug.Log("Uh Oh I slipped");
+
+    }
+
+
+    IEnumerator MoveToOrigin()
+    {
+
+            yield return new WaitForSeconds(0.5f);
+        print(Time.time);
+
+            currentAngle = new Vector3(
+                Mathf.LerpAngle(currentAngle.x, targetAngle.x, Time.deltaTime),
+                Mathf.LerpAngle(currentAngle.y, targetAngle.y, Time.deltaTime),
+                Mathf.LerpAngle(currentAngle.z, targetAngle.z, Time.deltaTime));
+
+            transform.eulerAngles = currentAngle;
+
+    }
+
+    
+
+}
